0a0955da54a3c052e18f8fdb497310248166cc46
Dayvison <dayvison.dev@gmail.com>
2025-07-26 10:28:06 -0300

    add-config-newposts-filters

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,13 @@ import EntendaBits from "./pages/EntendaBits";
 function App() {
 
 const postsVagante = [{"_id":"651ef5c208acb22a54779b5f","title":"Porque subi ate o topo","About":"O que leva uma pessoa a subir ate o topo do Mount Everest?","post":0,"data":"2023-07-12T03:00:00.000Z","tumb":"https://onedrive.live.com/embed?resid=CCDB940572A31808%2159410&authkey=%21AGohXx6QZwDguZ0&width=562&height=562","route":"summitofgods","comments":[{"nome":"dayvison","comentario":"heelo worlds\n"}]},{"_id":"684cbdc04fab4d746596250b","title":"Entendendo Bits","About":"Uma breve introdução ao funcionamento dos bits, desde sua origem básica, em como se transformam em numeros e até em letras.","post":2,"tumb":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqSpRWHNq2ChNoiTq4C1c3EvixrWnzT9C6XzJwsVKa6OdOyiWQ5Gw8fJbNqWwamo4Gi6w","route":"entenda-bits"},{"_id":"684cbf224fab4d746596250f","title":"Os Primeiros Computadores","About":"A Historia dos primeiros computadores e como funcionavam, conhecendo as principais ideias, ferramentas e importantes obras que contribuiram para que os computadores pudessem ser construidos.","post":1,"tumb":"https://imgcdn.stablediffusionweb.com/2024/10/8/e1a396ef-05f4-4846-b514-60b63ad9d85a.jpg","route":"primeiros-computadores"}]
-    
-  return (
+const postsTechno = [{"_id":"651ef5c208acb22a54779b5f","title":"Programação é massa","About":"aprenda a programar usando o meu curso","post":3,"data":"2023-07-12T03:00:00.000Z","tumb":"https://onedrive.live.com/embed?resid=CCDB940572A31808%2159410&authkey=%21AGohXx6QZwDguZ0&width=562&height=562","route":"summitofgods","comments":[{"nome":"dayvison","comentario":"heelo worlds\n"}]},{"_id":"684cbdc04fab4d746596250b","title":"Entendendo Bits","About":"Uma breve introdução ao funcionamento dos bits, desde sua origem básica, em como se transformam em numeros e até em letras.","post":4,"tumb":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqSpRWHNq2ChNoiTq4C1c3EvixrWnzT9C6XzJwsVKa6OdOyiWQ5Gw8fJbNqWwamo4Gi6w","route":"entenda-bits"},{"_id":"684cbf224fab4d746596250f","title":"Os Primeiros Computadores","About":"A Historia dos primeiros computadores e como funcionavam, conhecendo as principais ideias, ferramentas e importantes obras que contribuiram para que os computadores pudessem ser construidos.","post":5,"tumb":"https://imgcdn.stablediffusionweb.com/2024/10/8/e1a396ef-05f4-4846-b514-60b63ad9d85a.jpg","route":"primeiros-computadores"}]
+const dataPosts=[postsTechno,postsVagante]  
+return (
     <>
     <BrowserRouter>
     <Routes>
-    <Route path='/vagante'   element={<Vagante data={postsVagante?[postsVagante]:[]} />} />
+    <Route path='/vagante'   element={<Vagante data={postsVagante ?dataPosts:[]} />} />
     
   
     <Route path='/Summitofgods' element={<Summitofgods></Summitofgods>} />
--- src/Vagante.tsx
+++ src/Vagante.tsx
@@ -1,37 +1,74 @@
-import Header from "./header"
+import { useState } from "react";
+import Header from "./header";
 
 function Vagante(data: any) {
-  const posts = data.data[0]
+  const [posts, setPosts] = useState([...data.data[0], ...data.data[1]]);
+  const [cor, setCor] = useState(['bg-gray-800', 'bg-gray-800']);
+
+  function Chancefilter(x: any) {
+    if (x === "tecnico" && posts !== data.data[1]) {
+      setPosts(data.data[1]);
+      setCor(['bg-blue-800', 'bg-gray-800']); // Técnico azul, Outros cinza
+    } else if (x === "outros" && posts !== data.data[0]) {
+      setPosts(data.data[0]);
+      setCor(['bg-gray-800', 'bg-blue-800']); // Técnico cinza, Outros azul
+    } else {
+      setPosts([...data.data[0], ...data.data[1]]);
+      setCor(['bg-gray-800', 'bg-gray-800']);
+    }
+  }
 
   return (
     <>
-   
       <div className="bg-[#323C59] w-screen h-screen">
-
-        
         <div className="pt-20 w-screen h-full overflow-x-hidden flex flex-col items-center">
-        <div>
+          <div>
+            <button
+              type="button"
+              className={`text-white ${cor[0]} hover:brightness-110 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+              onClick={() => Chancefilter("tecnico")}
+            >
+              Técnicos
+            </button>
+            <button
+              type="button"
+              className={`text-white ${cor[1]} hover:brightness-110 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}
+              onClick={() => Chancefilter("outros")}
+            >
+              Outros
+            </button>
+          </div>
 
-        <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
-        <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Dark</button>
-
+          {posts.length > 0 ? (
+            posts.map((e: any) => (
+              <a
+                href={"/" + e.route}
+                key={e.post}
+                className="m-5 w-[60vw] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+              >
+                <img
+                  className="object-cover rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+                  src={e.tumb}
+                  alt=""
+                />
+                <div className="flex w-[60vw] flex-col justify-between p-4 leading-normal">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {e.title}
+                  </h5>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {e.About}
+                  </p>
+                </div>
+              </a>
+            ))
+          ) : (
+            <h1>Carregando...</h1>
+          )}
         </div>
-        {posts.length>0?posts.map((e:any)=>(<a href={'/'+e.route} key={e.post} className="m-5 w-[60vw] flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-<img className="object-cover  rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={e.tumb} alt=""/>
-<div className="flex w-[60vw] flex-col justify-between p-4 leading-normal">
-  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{e.title}</h5>
-  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{e.About}</p>
-</div>
-</a>
-)):<h1>carregando</h1>
-
-
-}
-        </div>
-         <Header></Header>
+        <Header />
       </div>
     </>
-  )
+  );
 }
 
-export default Vagante
+export default Vagante;
