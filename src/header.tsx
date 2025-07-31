3fcc4fd9a09314c9661421558114475bdf2367cd
Dayvison <dayvison.dev@gmail.com>
2025-07-31 13:14:10 -0300

    update-color-header

--- src/header.tsx
+++ src/header.tsx
@@ -2,7 +2,7 @@ function Header() {
 return(
 <>
 <header>
-    <nav className="text-white absolute w-screen mt-[-100vh] bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+    <nav className="text-white absolute w-screen mt-[-100vh]  border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex flex-wrap justify-around items-center mx-auto max-w-screen-xl">
             <a href="/" className="flex items-center">
                 <img src="https://ik.imagekit.io/yfgcubiem1/blog/11602236_21004063-removebg-preview.png?updatedAt=1753783748172" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
